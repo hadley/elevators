07d500d772107b2b076ab1f9dc654da3d714bbf3
Hadley Wickham <h.wickham@gmail.com>
2026-03-05 14:01:29 -0600

    Use consistent naming scheme

--- clean.R
+++ clean.R
@@ -4,4 +4,6 @@ elevators <- read.csv("elevators.csv", skip = 1)
 elevators$X <- NULL
 elevators <- elevators[!is.na(elevators$BIN), ]
 
+names(elevators) <- tolower(gsub(".", "_", names(elevators), fixed = TRUE))
+
 write_parquet(elevators, "elevators.parquet")
