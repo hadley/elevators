c7910cf0da453a56e38ea170bd6301e706b793a7
Hadley Wickham <h.wickham@gmail.com>
2026-03-05 14:04:05 -0600

    Consistently represent missing values

--- clean.R
+++ clean.R
@@ -6,4 +6,12 @@ elevators$DV_DEVICE_STATUS_DESCRIPTION <- NULL
 
 names(elevators) <- tolower(gsub(".", "_", names(elevators), fixed = TRUE))
 
+# Replace "" with NA in character columns
+elevators[] <- lapply(elevators, function(x) {
+  if (is.character(x)) ifelse(x == "", NA, x) else x
+})
+
+# Replace 0 with NA in zip_code
+elevators$zip_code[elevators$zip_code == 0] <- NA
+
 write_parquet(elevators, "elevators.parquet")
